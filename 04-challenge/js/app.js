5bfd77510a2009f73b4ebdd96151b6f448a5898d
Frandy De La Cruz <frandyjosue@gmail.com>
2018-10-29 11:45:24 -0400

    Adding my fixes to the 04-Challenge

--- 04-challenge/js/app.js
+++ 04-challenge/js/app.js
@@ -8,66 +8,56 @@ new Vue({
         userAttack: 0,
         monsterAttack: 0
     },
-    methods: {
-        attack: function() {
-            this.userAttack = Math.round(Math.random() * 10);
-            this.monsterAttack = Math.round(Math.random() * 10);
-            this.userHealth -= this.userAttack;
-            this.monsterHealth -= this.monsterAttack;
-
-            this.log.push("Player hits Monster for " + this.userAttack);
-            this.log.push("Monster hits Player for " + this.monsterAttack);
-
-            if(this.userHealth <= 0) {
+    watch: {
+        userHealth: function(life) {
+            if(life <= 0) {
                 alert("Monster kills you! You lose!!");
-                this.giveUp();
-            }
-            else if(this.monsterHealth <= 0) {
-                alert("You killed Monster! You won!!");
-                this.giveUp();
+                this.endGame();
             }
         },
-        specialAttack: function() {
-            this.userAttack = Math.round(Math.random() * 20);
-            this.monsterAttack = Math.round(Math.random() * 10);
+        monsterHealth: function(life) {
+            if(life <= 0) {
+                alert("You killed Monster! You won!!");
+                this.endGame();
+            }
+        }
+    },
+    methods: {
+        attack: function() {
+            this.userAttack = this.getRandom(10);
+            this.monsterAttack = this.getRandom(10);
             this.userHealth -= this.monsterAttack;
             this.monsterHealth -= this.userAttack;
 
-            this.log.push("Player hits Monster for " + this.userAttack);
-            this.log.push("Monster hits Player for " + this.monsterAttack);
+            this.log.unshift("Player hits Monster for " + this.userAttack);
+            this.log.unshift("Monster hits Player for " + this.monsterAttack); 
+        },
+        specialAttack: function() {
+            this.userAttack = this.getRandom(20);
+            this.monsterAttack = this.getRandom(10);
+            this.userHealth -= this.monsterAttack;
+            this.monsterHealth -= this.userAttack;
 
-            if(this.userHealth <= 0) {
-                alert("Monster kills you! You lose!!");
-                this.giveUp();
-            }
-            else if(this.monsterHealth <= 0) {
-                alert("You killed Monster! You won!!");
-                this.giveUp();
-            }
+            this.log.unshift("Player hits Monster for " + this.userAttack);
+            this.log.unshift("Monster hits Player for " + this.monsterAttack);
         },
         heal: function() {
-            this.userAttack = Math.round(Math.random() * 10);
-            this.monsterAttack = Math.round(Math.random() * 10);
+            this.userAttack = this.getRandom(10);
+            this.monsterAttack = this.getRandom(10);
             this.userHealth += this.userAttack;
             this.userHealth -= this.monsterAttack;
 
-            this.log.push("Player heals himself for " + this.userAttack);
-            this.log.push("Monster hits Player for " + this.monsterAttack);
-
-            if(this.userHealth <= 0) {
-                alert("Monster kills you! You lose!!");
-                this.giveUp();
-            }
-            else if(this.monsterHealth <= 0) {
-                alert("You killed Monster! You won!!");
-                this.giveUp();
-            }
+            this.log.unshift("Player heals himself for " + this.userAttack);
+            this.log.unshift("Monster hits Player for " + this.monsterAttack);
         },
-        giveUp: function() {
+        endGame: function() {
             this.gameStarted = !this.gameStarted;
             this.userHealth = 100;
             this.monsterHealth = 100;
             this.log = [];
+        },
+        getRandom: function(value) {
+            return Math.round(Math.random() * value);
         }
     }
 });
--- README.md
+++ README.md
@@ -13,8 +13,9 @@ This is a vue course development for learning purposes
 * Computed get executed when its dependent property change
 * Watch get executed when a specified property change and reveive the value of the property by default as a parameter
 
-Watch does perform asynchronous code, unlike Computed, that doesn't support it
+Watch does perform asynchronous code, unlike Computed, that doesn't support it.
+Watch method watchs the changes on specific properties | Computed methods needs to be render (or be present in the HTML) in the HTML
 
 ### HTML5 features
 
-<template> is a tag that doesn't show as text and is usefull to group certain code like a div does.
+`<template>` is a tag that doesn't show as text in the HTML and is usefull to group up certain code that has to be hidden like a div does.
